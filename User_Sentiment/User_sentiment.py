045b958c717283990e46743cd8a55a060fd936c8
Shubh Dhyani <42299658+sd411@users.noreply.github.com>
2021-03-07 17:07:54 +0530

    Add files via upload

---
+++ User_Sentiment/User_sentiment.py
@@ -0,0 +1,45 @@
+import random
+import os
+from flask import Flask, request, jsonify, Response, render_template
+from transformers import pipeline
+from transformers import AutoTokenizer, AutoModel,AutoModelForSequenceClassification
+import numpy as np
+
+app = Flask(__name__)
+
+tokenizer = AutoTokenizer.from_pretrained("facebook/bart-large-mnli")
+model = AutoModelForSequenceClassification.from_pretrained("facebook/bart-large-mnli")
+zsc = pipeline(task='zero-shot-classification', tokenizer=tokenizer, model=model)
+
+
+@app.route("/pos_neg",methods=["POST"])
+def get_pos_neg_score():
+    data = request.json
+    query = data["query"]
+    #print(str(query))
+    classes = ["positive","neutral","negative"]
+    results = zsc(sequences=str(query), candidate_labels=classes, multi_class=False)
+    print(results)
+    n= np.argmax(results["scores"])
+    sent={}
+    for i in range(len(results["scores"])):
+        sent[results["labels"][i]] = results["scores"][i]
+    return jsonify(sent)
+    
+    
+@app.route("/tonality",methods=["POST"])
+def get_tonality():
+    data = request.json
+    query = data["query"]
+    print(str(query))
+    classes = ["curious","interested","irrelevant","uninterested","indifferent"]
+    results = zsc(sequences=str(query), candidate_labels=classes, multi_class=False)
+    n= np.argmax(results["scores"])
+    sent={}
+    for i in range(len(results["scores"])):
+        sent[results["labels"][i]]= results["scores"][i]
+    return jsonify(sent)
+    
+    
+if __name__ == "__main__":
+    app.run()
